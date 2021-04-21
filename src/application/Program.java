db85ede08eaeb0e3820d14000b00dea6d27af872
Vinicius Pires <viniciusjps15@gmail.com>
2021-04-21 08:33:32 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world!");
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
 		
 	}
 
---
+++ src/boardgame/Position.java
@@ -0,0 +1,33 @@
+package boardgame;
+
+public class Position {
+
+	private int row;
+	private int colum;
+	
+	public Position(int row, int colum) {
+		this.row = row;
+		this.colum = colum;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public void setRow(int row) {
+		this.row = row;
+	}
+	
+	public int getColum() {
+		return colum;
+	}
+	
+	public void setColum(int colum) {
+		this.colum = colum;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + colum;
+	}
+}
