80b45d7e17ed032020b03140ec0b621023398956
Vinicius Pires <viniciusjps15@gmail.com>
2021-04-22 09:26:54 -0300

    Posible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -37,6 +37,9 @@ public class ChessMatch {
 	private void validateSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position))
 			throw new ChessException("There is no piece on source position");
+		
+		if(!board.piece(position).isThereAnyPossibleMove())
+			throw new ChessException("There is no possible moves for the chosen piece");
 	}
 	
 	private Piece makeMove(Position source, Position target) {
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
 
 	private Color color;
 
@@ -15,6 +15,5 @@ public class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
-	
 	
 }
