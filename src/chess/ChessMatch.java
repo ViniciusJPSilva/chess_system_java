84381fb393a9a5976361dc941780cd7b63980fb9
Vinicius Pires <viniciusjps15@gmail.com>
2021-04-24 08:59:03 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -101,7 +101,9 @@ public class ChessMatch {
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece) board.removePiece(source);
+		p.increaseMoveCount();
+		
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		
@@ -114,7 +116,9 @@ public class ChessMatch {
 	}
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece) board.removePiece(target);
+		p.decreaseMoveCount();
+		
 		board.placePiece(p, source);
 		
 		if(capturedPiece != null) {
