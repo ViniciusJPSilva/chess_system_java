a6b94007d1095ccfe3d973ed5eecfa663b13941a
Vinicius Pires <viniciusjps15@gmail.com>
2021-04-24 08:51:40 -0300

    Checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while(true) {
+		while(!chessMatch.getCheckMate()) {
 			try {	
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -48,6 +48,9 @@ public class Program {
 				scanf.nextLine();
 			}
 		}
+		
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -60,10 +60,16 @@ public class UI {
 		
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 		
-		if(chessMatch.getCheck())
-			System.out.println("CHECK!");
+		if(!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			
+			if(chessMatch.getCheck())
+				System.out.println("CHECK!");
+		}else {
+			System.out.println("\nCHECKMATE!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
