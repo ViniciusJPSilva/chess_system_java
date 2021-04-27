8a956e7c24e8e0a882efdeffa8f62fffe7dd0485
Vinicius Pires <viniciusjps15@gmail.com>
2021-04-27 09:34:39 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,13 @@ public class Program {
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 				if(capturedPiece != null)
 					captured.add(capturedPiece);
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = scanf.nextLine();
+					
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch(ChessException e) {
 				System.out.print(e.getMessage());
